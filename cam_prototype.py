4a361b21c029d15c217aa608e5407968c9d729d4
Joel Vander Klipp <joel.vanderklipp@gmail.com>
2017-09-26 13:48:03 -0400

    initial commit. prototype that captures b/w image

---
+++ cam_prototype.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+cap = cv2.VideoCapture(0)
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Display the resulting frame
+    cv2.imshow('frame',gray
+    s, im = cap.read() # captures image
+    im = imread(im, CV_LOAD_IMAGE_GRAYSCALE)
+    cv2.imwrite("test.bmp",im) # writes image test.bmp to disk
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
+
+
