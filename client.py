8383c292774ab62d275fd651357d3aaaa977f1a9
Joel Vander Klipp <joel.vanderklipp@gmail.com>
2017-10-24 18:42:18 -0400

    overwrote prototype server and client with my copies

--- client.py
+++ client.py
@@ -1,28 +1,91 @@
-#client
+import keras
 import socket
+import threading
 import sys
+import os
+import signal
 
-#Create UDP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+BUFF_SIZE = 1500
 
-server_address = ('localhost', 20000)
-message = 'This is the message. It will be repeated.'
+class Quit(Exception):
+    pass
 
-while True:
-	print >>sys.stderr, 'Please enter a message: '
-	msg = raw_input()
-	
-	if msg == 'Exit':
-		print >>sys.stderr, 'Goodbye'
-		sock.close()
-		break
-	else:
-		#send data
-		print >>sys.stderr, 'sending "%s"' % msg
-		sent = sock.sendto(msg, server_address)
 
-		#receive response
-		print >>sys.stderr, 'waiting for receive'
-		data, server = sock.recvfrom (4096)
-		print >>sys.stderr, 'received "%s"' % data
+class Killed(Exception):
+    pass
 
+
+def exit_signal(signal, frame):
+    raise Quit
+
+
+def killed_signal(signal, frame):
+    raise Killed
+
+
+def main():
+    value = True
+    while value:
+        server_ip = get_input("IP: ")
+        port_num = get_input("Port: ")
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((server_ip, int(port_num)))
+        signal.signal(signal.SIGUSR1, exit_signal)
+        signal.signal(signal.SIGUSR2, killed_signal)
+
+        messaging = threading.Thread(target=server_handler, args=(s,))
+        messaging.daemon = True
+        messaging.start()
+
+        try:
+            while True:
+                msg = get_input("[]$ ")
+                s.send(("msg" + msg).encode('utf-8'))
+                # this output goes to UI to be printed
+
+                if msg[3:7] == "exit" or not msg:
+                    os.kill(os.getpid(), signal.SIGUSR1)
+
+        except Quit:
+            value = False
+        except Killed:
+            pass
+        finally:
+            s.close()
+
+def server_handler(sock):
+    while True:
+        encodedmsg = sock.recv(BUFF_SIZE)
+        msg = encodedmsg.decode('utf-8')
+        if msg[:3] == 'msg':
+            print('\n' + msg[3:])
+            sys.stdout.flush()
+            sys.stdout.write("[]$ ")
+            sys.stdout.flush()
+            # this output goes to UI to be printed
+
+        if msg[:3] == 'lst':
+            print('\n' + msg[3:])
+            sys.stdout.flush()
+            sys.stdout.write("[]$ ")
+            sys.stdout.flush()
+
+        if msg[:3] == 'emo':
+            # interface with the UI to display image
+            print("emo: " + msg)
+
+        if msg[3:7] == "exit":
+            sock.send(("msg" + msg).encode('utf-8'))
+            print('\n')
+            os.kill(os.getpid(), signal.SIGUSR2)
+            return
+
+        
+def get_input(msg):
+    sys.stdout.write(msg)
+    sys.stdout.flush()
+    input = sys.stdin.readline()
+    return input
+
+if __name__ == "__main__":
+    main()
