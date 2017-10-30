7a53d10db969a78301e6dc48f1efb033bb4aa1ae
Emily Wang <wangem@mail.gvsu.edu>
2017-10-30 16:09:53 -0400

    Added to GUI

--- GUI.py
+++ GUI.py
@@ -2,16 +2,32 @@ from Tkinter import *
 
 root = Tk()
 
-msgBox = Message(root, text = "Text Here")
-msgBox.config(font = ('times', 24))
-msgBox.pack(padx = 30, pady = 50, side = TOP)
+messages = Text(root)
+messages.pack()
 
-Label(root, text = "Message: ").pack(padx = 10, pady = 20, side = LEFT)
+input_user = StringVar()
+input_field = Entry(root, text=input_user)
+input_field.pack(side=BOTTOM, fill=X)
+
+def send(event):
+	input_get = input_field.get()
+	if input_get:
+		messages.insert(INSERT, 'Me: %s\n' % input_get)
+		input_user.set('')
+		return "break"
+
+def sendButton():
+	input_get = input_field.get()
+	if input_get:
+		messages.insert(INSERT, 'Me: %s\n' % input_get)
+		input_user.set('')
+
+Label(root, text= "Message: ").pack(padx = 10, pady = 20, side = LEFT)
 enter = Text(root, height = 1, width = 50, wrap = WORD)
 enter.pack(padx = 20, pady = 20, side = LEFT)
 
-
-but = Button(root, text = "Send")
+input_field.bind("<Return>", send)
+but = Button(root, text = "Send", command=sendButton)
 but.pack(padx = 20, pady = 20, side = LEFT)
 #but.place(width = 120, height = 40)
 root.mainloop()
